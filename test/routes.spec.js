c9071f855967da868f368b33e3e09f6df3b82aea
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-30 11:54:34 -0700

    Add a sad path test.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -16,7 +16,17 @@ describe('Client Routes', () => {
       done();
     });
   });
+  
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
 });
+
 
 describe('API Routes', () => {
 
