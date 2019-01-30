f81a38ff8f27a2a974a3bd0eea052fa3aeab0f4e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-30 11:49:50 -0700

    Test that it has a home page.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,7 +6,16 @@ const server = require('../index');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage with text', done => {
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('Welcome to Calorie Tracker');
+      done();
+    });
+  });
 });
 
 describe('API Routes', () => {
