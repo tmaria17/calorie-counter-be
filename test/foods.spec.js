c3d027608b8d12fb1ed7550caeb9cc7d37668914
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-31 14:01:13 -0700

    Add test for specific food route.

--- test/foods.spec.js
+++ test/foods.spec.js
@@ -9,7 +9,7 @@ const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
-describe('API Routes', () => {
+describe('API Routes for Foods', () => {
   before((done) => {
     database.migrate.latest()
       .then(() => done())
@@ -27,21 +27,37 @@ describe('API Routes', () => {
   });
 
   describe('GET /api/v1/foods', () => {
- it('should return all of the foods', done => {
-    chai.request(server)
-      .get('/api/v1/foods')
-      .end((err, response) => {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.be.a('array');
-        response.body.length.should.equal(3);
-        response.body[0].should.have.property('name');
-        response.body[0].name.should.equal('Burger');
-        response.body[0].should.have.property('calories');
-        response.body[0].calories.should.equal(365);
-        done();
-      });
+    it('should return all foods', done => {
+       chai.request(server)
+         .get('/api/v1/foods')
+         .end((err, response) => {
+           response.should.have.status(200);
+           response.should.be.json;
+           response.body.should.be.a('array');
+           response.body.length.should.equal(3);
+           response.body[0].should.have.property('name');
+           response.body[0].should.have.property('calories');
+           response.body[0].name.should.equal('Burger');
+           response.body[0].calories.should.equal(365);
+           done();
+         });
+       });
+     });
+
+  describe('GET /api/v1/foods/:id', () => {
+    it('should return a specific food', done => {
+       chai.request(server)
+         .get('/api/v1/foods/2')
+         .end((err, response) => {
+           response.should.have.status(200);
+           response.should.be.json;
+           response.body.should.be.a('object');
+           response.body.should.have.property('name');
+           response.body.should.have.property('calories');
+           response.body.name.should.equal('Yogurt');
+           response.body.calories.should.equal(100);
+           done();
+         });
+       });
     });
   });
-
-});
