2036f066b014584447560334ab33a7ee0fdbfc93
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-31 17:52:24 -0700

    Add route for deleting food.

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ app.patch('/api/v1/foods/:id', (request, response) => {
     });
 });
 
-
+app.delete('/api/v1/foods/:id', (request, response) => {
+  database('foods').where('id', request.params.id).del()
+  .then(foods => {
+    if (foods == 1) {
+      response.status(204).json({success: true});
+    } else {
+      response.status(404).json({ error });
+    }
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+});
 
 module.exports = app
