d7022b4f06c89cef37594c063210332e04277880
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-01 22:49:17 -0700

    Add test and functionality for Meal foods endpoint

--- test/meals.spec.js
+++ test/meals.spec.js
@@ -28,20 +28,38 @@ describe('API Routes for Meals', () => {
 
   describe('GET /api/v1/foods', () => {
     it('should return all foods', done => {
-       chai.request(server)
-         .get('/api/v1/foods')
-         .end((err, response) => {
-           response.should.have.status(200);
-           response.should.be.json;
-           response.body.should.be.a('array');
-           response.body.length.should.equal(3);
-           response.body[0].should.have.property('name');
-           response.body[0].should.have.property('calories');
-           response.body[0].name.should.equal('Burger');
-           response.body[0].calories.should.equal(365);
-           done();
-         });
+      chai.request(server)
+       .get('/api/v1/foods')
+       .end((err, response) => {
+         response.should.have.status(200);
+         response.should.be.json;
+         response.body.should.be.a('array');
+         response.body.length.should.equal(3);
+         response.body[0].should.have.property('name');
+         response.body[0].should.have.property('calories');
+         response.body[0].name.should.equal('Burger');
+         response.body[0].calories.should.equal(365);
+         done();
        });
      });
+   });
+
+  describe('GET /api/v1/meals/:meal_id/foods', () => {
+    it('should return an array of meals with arrays of food objects', done => {
+      chai.request(server)
+      .get('/api/v1/meals/1/foods')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+         response.body.should.have.property('id');
+         response.body.should.have.property('meal');
+         response.body.should.have.property('foods');
+         response.body.foods[0].should.have.property('id');
+         response.body.foods[0].should.have.property('name');
+         response.body.foods[0].should.have.property('calories');
+         done();
+        });
+      });
+    })
 
 });
